badd4be5a054db332885673e8976e78a8e49fc71
Nikolay <bychkevich.n@gmail.com>
2022-01-25 12:02:49 +0300

    React-Lesson2-add

---
+++ react-lesson2/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import ButtonLink from "./components/ButtonLink/ButtonLink";
+import Profile from "./components/Profile/Profile";
+import ProfileForm from "./components/ProfileForm/ProfileForm";
+
+
+const TEST_DATA = {
+  firstName: 'Alex',
+  lastName: 'John',
+  photoSrc: 'https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg',
+  hobbies: [
+    {id: '1', name: 'guitar'},
+    {id: '2', name: 'naga'},
+    {id: '3', name: 'akita'},
+  ],
+};
+
+const UserProfileContainerStyles = {
+  display: 'flex',
+}
+const App = () => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleEdit = () => {
+    setIsEdit(true);
+  };
+
+  const handleSave = () => {
+    setIsEdit(false);
+  }
+  return (
+    <div>
+      <div style={UserProfileContainerStyles}>
+        User Profile{' '}
+        <ButtonLink onClick={isEdit ? handleSave : handleEdit}>
+          {isEdit ? 'Save' : 'Edit'}
+          </ButtonLink>
+      </div>
+
+      {!isEdit && <Profile user={TEST_DATA} />}
+      {isEdit && <ProfileForm user={TEST_DATA} />}
+    </div>
+  );
+};
+
+export default App;
